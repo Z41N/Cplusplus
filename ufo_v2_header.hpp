84ecec79028b772fe9083b8bab1bd813c20b9097
Zain <zain.sheraz@gmail.com>
2020-06-12 11:31:16 -0400

    UFO v2 header

---
+++ ufo_v2_header.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+
+void greet();
+bool end_game(string phrase, string dashes);
+void display_misses(int misses);
